1a0b736f3bcb05e49c09ccb1f71cdaa8abd3aef5
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2023-01-23 09:27:02 +0300

    remove misleading comment

--- binary-trees/maxDepth.js
+++ binary-trees/maxDepth.js
@@ -1,7 +1,5 @@
 const maxDepth = (root) => {
   if (!root) return 0;
 
-  // Note: Depth is Tree Height + 1 of the longest side;
-
   return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
